2839388ee25a1880bed1c2578f5497c44577cdad
Fernando Mayer <fernandomayer@gmail.com>
2015-03-19 18:03:59 -0300

    simplificando para o mint

--- run-after-install.sh
+++ run-after-install.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 echo -------------------------------------------------------------------
-echo Instalar Emacs 24? [ 1/0 ]
+echo Instalar Emacs e Vim? [ 1/0 ]
 echo -------------------------------------------------------------------
-# De https://launchpad.net/~cassou/+archive/emacs
-# Para versoes antigas apenas
 read opcao
 if [ $opcao -eq 1 ] ; then
-    # add-apt-repository -y ppa:cassou/emacs
-    # apt-get update
-    apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg \
-	emacs-goodies-el
+    sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg \
+	 emacs-goodies-el vim
 fi
 
 echo -------------------------------------------------------------------
@@ -18,12 +14,12 @@ echo Instalar LaTeX? [ 1/0 ]
 echo -------------------------------------------------------------------
 read opcao
 if [ $opcao -eq 1 ] ; then
-    apt-get install -y texlive-base texlive-bibtex-extra \
-	texlive-fonts-extra texlive-fonts-recommended \
-	texlive-generic-extra texlive-latex-extra texlive-math-extra \
-	texlive-plain-extra texlive-science latex-xcolor latex-beamer \
-	texlive-lang-portuguese \
-	cm-super cm-super-minimal cm-super-x11 auctex
+    sudo apt-get install -y texlive-base texlive-bibtex-extra \
+	 texlive-fonts-extra texlive-fonts-recommended \
+	 texlive-generic-extra texlive-latex-extra texlive-math-extra \
+	 texlive-plain-extra texlive-science latex-xcolor latex-beamer \
+	 texlive-lang-portuguese \
+	 cm-super cm-super-minimal cm-super-x11 auctex
 fi
 
 echo -------------------------------------------------------------------
@@ -31,8 +27,8 @@ echo Instalar fontes extras? [ 1/0 ]
 echo -------------------------------------------------------------------
 read opcao
 if [ $opcao -eq 1 ] ; then
-    apt-get install -y ttf-bitstream-vera ttf-dejavu fonts-inconsolata \
-	ttf-liberation fonts-linuxlibertine 
+    sudo apt-get install -y ttf-bitstream-vera ttf-dejavu fonts-inconsolata \
+	 ttf-liberation fonts-linuxlibertine 
 fi
 
 echo -------------------------------------------------------------------
@@ -77,8 +73,7 @@ echo Instalar pacotes de produtividade? [ 1/0 ]
 echo -------------------------------------------------------------------
 read opcao
 if [ $opcao -eq 1 ] ; then
-    sudo apt-get install -y dia \
-	maxima wxmaxima gnuplot
+    sudo apt-get install -y dia maxima wxmaxima gnuplot
 fi
 
 echo -------------------------------------------------------------------
@@ -86,29 +81,7 @@ echo Instalar pacotes multimidia? [ 1/0 ]
 echo -------------------------------------------------------------------
 read opcao
 if [ $opcao -eq 1 ] ; then
-    # E o resultado de apt-get install ubuntu-restricted-extras,
-    # mas tirei cabextract e ttf-mscorefonts-installer
-    apt-get install -y gstreamer0.10-plugins-bad-multiverse \
-        libavcodec-extra-54 libav-tools libavutil-extra-52 libfaac0 \
-        libmjpegutils-2.1-0 libmpeg2encpp-2.1-0 libmplex2-2.1-0 \
-        libopenjpeg2 unrar xfce4-mixer vlc
-fi
-
-echo -------------------------------------------------------------------
-echo Instalar Medibuntu e libdvdcss2? [ 1/0 ]
-echo -------------------------------------------------------------------
-read opcao
-if [ $opcao -eq 1 ] ; then
-    echo Baixando Medibuntu e colocando em /etc/apt/sources.list.d/
-    echo
-    wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list 
-    apt-get update 
-    apt-get --yes --allow-unauthenticated install medibuntu-keyring 
-    apt-get update
-    echo
-    echo
-    echo Instalando libdvdcss2
-    apt-get install -y libdvdcss2
+    sudo apt-get install -y ubuntu-restricted-extras
 fi
 
 echo -------------------------------------------------------------------
